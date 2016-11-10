37a9209a34e0c99b1a5d139b985fdfa3dd007b80
Your Name <mike@datsko.it>
2016-11-10 10:52:51 +0200

    mongoose fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
 
